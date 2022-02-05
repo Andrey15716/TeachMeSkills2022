da8c1a112cbf06338fdf8b4968eecddc1334c644
Andrey15716 <52579943+Andrey15716@users.noreply.github.com>
2022-02-05 18:19:11 +0300

    Lessson 7 (ошибка)

---
+++ Lesson7/src/main/java/Address.java
@@ -0,0 +1,26 @@
+public class Address {
+
+    private String country;
+    private String city;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
