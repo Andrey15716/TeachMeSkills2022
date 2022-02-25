4baf087d3de067c6d18a422222bba76558c9941e
Andrey15716 <52579943+Andrey15716@users.noreply.github.com>
2022-02-25 16:11:05 +0300

    Delete Main.java

--- Lesson13/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuffer stringBuffer = new StringBuffer("hello");
-        stringBuffer.append("world");
-        System.out.println(stringBuffer);
-    }
-}
