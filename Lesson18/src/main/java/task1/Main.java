4572556f4f682da92e72b42951b33c3b1ebf06fb
Andrey15716 <1706001699601@mail.ru>
2022-03-31 11:40:33 +0300

    Lesson 18,19

--- Lesson18/src/main/java/task1/Main.java
+++ Lesson18/src/main/java/task1/Main.java
@@ -5,26 +5,21 @@ package task1;
 //        * Создать коллекцию, содержащую объекты класса Student. Перебрать всех студентов, вычислить средний балл
 //        * каждого студента. Если средний балл >=3, студент переводится на следующий курс, иначе отчисляется.
 
-import java.util.IntSummaryStatistics;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         List<Student> studentList = List.of(
-                new Student("Andrey", "Informatics", 3, 2, 1, 4),
-                new Student("Ivan", "Informatics", 3, 2, 3, 4),
-                new Student("Vladimir", "Informatics", 4, 3, 4, 5),
-                new Student("Vlad", "Informatics", 2, 4, 5, 6)
+                new Student("Andrey", 2, Arrays.asList(1, 2, 3, 4, 4)),
+                new Student("Ivan", 3, Arrays.asList(2, 3, 4, 5)),
+                new Student("Vladimir", 3, Arrays.asList(5, 4, 3, 2, 2)),
+                new Student("Vlad", 4, Arrays.asList(4, 4, 4, 2, 1, 8))
         );
-        List<Integer> integerList = studentList.stream().mapToInt(Student::getEstimateForMathematics).boxed().toList();
-        double avg = getAverage(integerList);
-        System.out.println(avg);
-    }
 
-    private static double getAverage(List<Integer> list) {
-        IntSummaryStatistics stats = list.stream()
-                .mapToInt(Integer::intValue)
-                .summaryStatistics();
-        return stats.getAverage();
+        Map<String, Double> averageScores = Student.getAverageScoresOfStudents(studentList);
+        System.out.println(averageScores);
+        Student.studentsInfo(studentList);
     }
 }
