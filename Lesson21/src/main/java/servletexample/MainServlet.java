1232afa24b409e0706324fae38c0f6a7cb928569
Andrey15716 <1706001699601@mail.ru>
2022-04-07 15:18:11 +0300

    Lesson 21

---
+++ Lesson21/src/main/java/servletexample/MainServlet.java
@@ -0,0 +1,33 @@
+package servletexample;
+
+import utils.Calculator;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/index")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String varTextA = String.valueOf(Calculator.sum(2, 4));
+        req.setAttribute("textA", varTextA);
+
+        String varTextB = String.valueOf(Calculator.multiply(2, 4));
+        req.setAttribute("textB", varTextB);
+
+        String varTextC = String.valueOf(Calculator.divide(4, 2));
+        req.setAttribute("textC", varTextC);
+
+        String varTextD = String.valueOf(Calculator.subtraction(3.3, 2));
+        req.setAttribute("textD", varTextD);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
---
+++ Lesson21/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user124
+  Date: 05.04.2022
+  Time: 13:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Калькулятор</title>
+</head>
+<body>
+<i>Сумма чисел равна - ${textA}</i><br>
+<i>Умножение двух чисел равно - ${textB}</i><br>
+<i>Деление двух чисел равно - ${textC}</i><br>
+<i>Вычитание двух чисел равно - ${textD}</i><br>
+</body>
+</html>
