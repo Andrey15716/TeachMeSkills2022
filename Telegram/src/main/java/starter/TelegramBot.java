f449f3f7a518f0994a9bef8eaa7bfd571ea75118
Andrey15716 <1706001699601@mail.ru>
2022-04-13 10:24:18 +0300

    TelegramBot

--- Telegram/src/main/java/starter/TelegramBot.java
+++ Telegram/src/main/java/starter/TelegramBot.java
@@ -2,17 +2,23 @@ package starter;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import starter.service.MessageServiceBot;
 
 public class TelegramBot extends TelegramLongPollingBot {
+
+    private final MessageServiceBot messageServiceBot;
 
     @Value("${bot.username}")
     private String username;
 
     @Value("${bot.token}")
     private String token;
+
+    public TelegramBot(MessageServiceBot messageServiceBot) {
+        this.messageServiceBot = messageServiceBot;
+    }
 
     @Override
     public String getBotUsername() {
@@ -26,20 +32,13 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String message = update.getMessage().getText().trim();
-            String chatId = update.getMessage().getChatId().toString();
-
-            SendMessage sm = new SendMessage();
-            sm.setChatId(chatId);
-            sm.setText(message);
-
-            try {
-                execute(sm);
-            } catch (TelegramApiException e) {
-                //todo add logging to the project.
-                System.out.println(e.getMessage());
+        try {
+            Message message1 = update.getMessage();
+            if (message1 != null && message1.hasText()) {
+                execute(messageServiceBot.inputMessage(message1));
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
